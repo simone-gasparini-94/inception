67904b887e70276fcb5627c3d9c1c19fd1c2a51e
sgaspari <sgaspari@vbox.42berlin.de>
2026-03-17 14:39:53 +0100

    Add .env file and set script as entrypoint for nginx service

---
+++ srcs/requirements/nginx/tools/nginx_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout /etc/ssl/private/nginx.key \
+        -out /etc/ssl/certs/nginx.crt \
+        -subj "/CN=${DOMAIN_NAME}"
+
+exec    nginx -g 'daemon off;'
