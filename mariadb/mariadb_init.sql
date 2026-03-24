6e2384b5cbd179e63cefc42faf7cd837e362a056
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2026-03-24 13:00:56 +0100

    Run a sql script in docker-entypoint-initdb.d

---
+++ mariadb/mariadb_init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS wordpress;
+
+CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY 'wordpresspass';
+
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';
+
+FLUSH PRIVILEGES;
